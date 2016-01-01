8e7728b052caea7e5bde8fba19c79c031b7bbc7d
HS <none@email.none>
2016-01-01 21:39:55 +0000

    .

--- 01 Don't Repeat Yourself/Comparator.java
+++ 01 Don't Repeat Yourself/Comparator.java
@@ -2,7 +2,7 @@
  * Look at the following code. Is there anything you can do to make this code
  * better? Hint: you may need to convert between types (e.g. casting).
  */
-
+/*
 public class Comparator {
 
 	public int getMax(int n, int m) {
@@ -24,14 +24,38 @@ public class Comparator {
 	}
 
 	public String getMax(String number1, String number2) {
-		double n1 = Double.parseDouble(number1); // Changed to Double
-		double n2 = Double.parseDouble(number2); // Changed to Double
+		int n1 = Integer.parseInt(number1);
+		int n2 = Integer.parseInt(number2);
 		if(n1 > n2) {
 			return number1;
 		}
 		else {
 			return number2;
 		}
+	}
+
+}
+*/
+
+public class Comparator {
+
+	public int getMax(int n, int m) {
+		return (int) getMax((double) n, (double) m);
+	}
+
+	public double getMax(double d1, double d2) {
+		if(d1 > d2) {
+			return d1;
+		}
+		else {
+			return d2;
+		}
+	}
+
+	public String getMax(String number1, String number2) {
+		int n1 = Integer.parseInt(number1);
+		int n2 = Integer.parseInt(number2);
+		return Integer.toString(getMax(n1, n2));
 	}
 
 }
