ab0c247aacff277e11f527b1466aaac663447cc7
BBK <PiJ>
2016-01-31 17:32:18 +0000

    Changed with IntelliJ.

--- 06 Generic Maps - Simple Map/src/Map.java
+++ 06 Generic Maps - Simple Map/src/Map.java
@@ -7,7 +7,7 @@ public class Map {
     public static void main(String[] args) {
 
         for(int i = 0; i < 5; i++) {
-            System.out.println("Simple Map!");
+            System.out.println("Simple Map!!!");
         }
 
     }
